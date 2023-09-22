bea25b6bec59433117acc57d79c63fa554366cb4
Anastasiia Ivanova <anastasiia1ivanova1@gmail.com>
2023-09-22 01:36:14 +0000

    made a name regeneration

--- src/app.js
+++ src/app.js
@@ -12,9 +12,15 @@ for (let p = 0; p < pronoun.length; p++) {
   for (let a = 0; a < adj.length; a++) {
     for (let n = 0; n < noun.length; n++) {
       // console.log(`${pronoun[p]}${adj[a]}${noun[n]}.com`);
-      let div = document.createElement("div");
-      div.innerText = `${pronoun[p]}${adj[a]}${noun[n]}.com`;
-      document.body.appendChild(div);
+      // let div = document.createElement("div");
+      // div.innerText = `${pronoun[p]}${adj[a]}${noun[n]}.com`;
+      // document.body.appendChild(div);
     }
   }
 }
+let pronounIdx = [Math.floor(Math.random() * pronoun.length)];
+let adjIdx = [Math.floor(Math.random() * adj.length)];
+let nounIdx = [Math.floor(Math.random() * noun.length)];
+const resultMessage = `${pronoun[pronounIdx]}${adj[adjIdx]}${noun[nounIdx]}.com`;
+//runSentence
+document.getElementById("DomGenerator").innerHTML = resultMessage;
